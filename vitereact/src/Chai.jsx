1fcb15e84466cfb30790bcd4b2d7e6af5b4fa701
Rakesh Kumar Dhiwar <134353813+RakeshDhiwar@users.noreply.github.com>
2024-03-29 16:43:39 +0000

    new component created

---
+++ vitereact/src/Chai.jsx
@@ -0,0 +1,18 @@
+// Note
+// * Name of component must start with capital letters
+
+// * Try to name of the file as same as component
+
+// * If you are using vite components must be saved with '.jsx' extension
+
+// * No need of '.jsx' in 'create-react-app' you can use '.js' also for components but naming convention must be followed
+
+function Chai() {
+    return(
+        <>
+            <h1>Chai Ready</h1>
+        </>
+    )
+}
+
+export default Chai
